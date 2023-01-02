482eb37fd0321cc2c88522b0146f77c20f50acad
unchainedDavid <afonso.script.sol@gmail.com>
2023-01-02 13:25:18 -0400

    Initial Commit

---
+++ GiftCardAnimation/GiftCardAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftCardAnimationApp.swift
+//  GiftCardAnimation
+//
+//  Created by Afonso Lucas on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GiftCardAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
